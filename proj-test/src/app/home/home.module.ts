ec64c34c2360aebf543c3f79c7003925d6e3f829
VLNohai <vladnohai@gmail.com>
2023-05-12 23:20:36 +0300

    changes to homepage

--- proj-test/src/app/home/home.module.ts
+++ proj-test/src/app/home/home.module.ts
@@ -4,16 +4,20 @@ import { HomeComponent } from './home/home.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { PostComponent } from './post/post.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    PostComponent
+    PostComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule,
     NzButtonModule,
-    NzIconModule
+    NzIconModule,
+    SearchModule
   ]
 })
 export class HomeModule { }
---
+++ proj-test/src/app/home/search/search.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search/search.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { ResultComponent } from './result/result.component';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
+@NgModule({
+  declarations: [
+    SearchComponent,
+    ResultComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SearchComponent,
+    NzInputModule,
+    NzButtonModule,
+    NzDropDownModule
+  ]
+})
+export class SearchModule { }
